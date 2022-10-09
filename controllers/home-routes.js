a4b8cbc4b2ac05870f8d858e51f96b8d40b8709c
ejackson1228 <ejackson1228@gmail.com>
2022-10-09 15:59:59 -0400

    added homepage populated with posts from db using handlbars

---
+++ controllers/home-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true })); // map the array of posts and serialize the object down to only needed properties
+        res.render('homepage', { posts }); 
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
 
 const apiRoutes = require('./api');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => { //indicates a bad request and sends an error to let the client know they made a bad request
