0a00734ef3be5d22275d2ab299deb7d56750075b
ejackson1228 <ejackson1228@gmail.com>
2022-10-11 16:58:37 -0400

    added logout for session destroy

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -148,6 +148,17 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+//logout
+router.post('/logout',  (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(()=>{
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+
+});
 
 module.exports = router;
 
